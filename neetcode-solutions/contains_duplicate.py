3ab03c79a15a760ed6137f78dcbadceeba0e9dfe
vishnu-git-lab <vishnu.work.4634@gmail.com>
2026-08-30 14:25:31 +0530

    Delete neetcode-solutions/contains_duplicate.py

--- neetcode-solutions/contains_duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        ax = set()
-        for num in nums:
-            if num in ax:
-                return True
-            ax.add(num)
-        return False
