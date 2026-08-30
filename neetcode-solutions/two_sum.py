25c135e04e4b4cc431d0cf6a5aa27de01b57f6db
vishnu-git-lab <vishnu.work.4634@gmail.com>
2026-08-30 14:25:49 +0530

    Delete neetcode-solutions/two_sum.py

--- neetcode-solutions/two_sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ai = {}
-        for i, num in enumerate(nums):
-            q = target - num
-            if q in ai:
-                return [ai[q], i]
-            ai[num] = i
-        return []
